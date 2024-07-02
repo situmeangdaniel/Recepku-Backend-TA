b69dfefaa50a8658f7507fc9fd9b7b3b2fbd2abc
Daniel Situmeang <117498954+danielstmg@users.noreply.github.com>
2024-07-03 03:21:26 +0700

    Update service account

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 var fire = require("firebase-admin");
 
-var serviceAccount = require("../db-recepku-firebase-adminsdk-s0rfd-037888428b.json");
+var serviceAccount = require("../db-recepku-firebase-adminsdk-s0rfd-3c2986487d.json");
 
 fire.initializeApp({
   credential: fire.credential.cert(serviceAccount)
