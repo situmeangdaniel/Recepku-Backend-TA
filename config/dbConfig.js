13a5cbdb0bc513ddf534797490e2f171bdc43a71
Daniel Situmeang <117498954+danielstmg@users.noreply.github.com>
2024-07-03 02:35:13 +0700

    fix issue

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 var fire = require("firebase-admin");
 
-var serviceAccount = require("../db-recepku-firebase-adminsdk-s0rfd-2866371068.json");
+var serviceAccount = require("../db-recepku-firebase-adminsdk-s0rfd-aa2d967659.json");
 
 fire.initializeApp({
   credential: fire.credential.cert(serviceAccount)
